59dc608008f851c73a1e1706edbadda34f069896
shienxiii@gmail.com <shienxiii@gmail.com>
2019-12-29 15:10:07 +0800

    Joining and hosting can be done using game terminal

--- Source/HIVE/System/Game Instance/HiveGameInstance.cpp
+++ Source/HIVE/System/Game Instance/HiveGameInstance.cpp
@@ -10,10 +10,17 @@ void UHiveGameInstance::Host()
 	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Cyan, "Hosting");
 
 	UWorld* world = GetWorld();
+	if (!ensure(world != nullptr)) { return; }
+
 	world->ServerTravel("/Game/ThirdPersonBP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UHiveGameInstance::Join(const FString& InAddress)
 {
 	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Cyan, FString::Printf(TEXT("Joining %s"), *InAddress));
+
+	APlayerController* controller = GetFirstLocalPlayerController();
+	if (!ensure(controller != nullptr)) { return; }
+
+	controller->ClientTravel(*InAddress, ETravelType::TRAVEL_Absolute);
 }
