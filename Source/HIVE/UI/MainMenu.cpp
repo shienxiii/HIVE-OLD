948e5347f0bda205a70d835a8f5210770529f395
shienxiii <shienpsn@gmail.com>
2020-01-17 19:41:25 +0800

    Working Host button

---
+++ Source/HIVE/UI/MainMenu.cpp
@@ -0,0 +1,26 @@
+// Copyright of Honeycomb Studio
+
+
+#include "MainMenu.h"
+#include "Components/Button.h"
+#include "HIVE/System/Game Instance/HiveGameInstance.h"
+
+bool UMainMenu::Initialize()
+{
+	Super::Initialize();
+
+	GameInstance = Cast<UHiveGameInstance>(GetGameInstance());
+	check(GameInstance);
+
+
+	bIsFocusable = true;
+
+	HostBtn->OnClicked.AddDynamic(this, &UMainMenu::HostClickEvent);
+
+	return true;
+}
+
+void UMainMenu::HostClickEvent()
+{
+	GameInstance->Host();
+}
---
+++ Source/HIVE/UI/MainMenu.h
@@ -0,0 +1,37 @@
+// Copyright of Honeycomb Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+class UHiveGameInstance;
+class UButton;
+
+/**
+ * 
+ */
+UCLASS()
+class HIVE_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+    UHiveGameInstance* GameInstance;
+
+    // A UPROPERTY with BindWidget metadata will automatically bind the button on the editor to the variable
+
+    UPROPERTY(meta = (BindWidget))
+        UButton* HostBtn;
+
+    UPROPERTY(meta = (BindWidget))
+        UButton* JoinBtn;
+
+public:
+    virtual bool Initialize() override;
+
+    UFUNCTION()
+        void HostClickEvent();
+
+};
