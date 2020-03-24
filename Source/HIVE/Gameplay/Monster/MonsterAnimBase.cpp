a7ae70c38146045d77febdebcace9af0071862c1
shienxiii@gmail.com <shienxiii@gmail.com>
2020-03-24 15:49:23 +0800

    test character locomotion script working

--- Source/HIVE/Gameplay/Monster/MonsterAnimBase.cpp
+++ Source/HIVE/Gameplay/Monster/MonsterAnimBase.cpp
@@ -2,4 +2,15 @@
 
 
 #include "MonsterAnimBase.h"
+#include "Kismet/KismetMathLibrary.h"
 
+void UMonsterAnimBase::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (!GetOwningActor()) { return; }
+
+	FVector OwnerVelocity = GetOwningActor()->GetVelocity();
+	FTransform OwnerTransform = GetOwningActor()->GetActorTransform();
+
+	ForwardAxis = FVector::DotProduct(GetOwningActor()->GetActorForwardVector(), OwnerVelocity);
+	RightAxis = FVector::DotProduct(GetOwningActor()->GetActorRightVector(), OwnerVelocity);
+}
--- Source/HIVE/Gameplay/Monster/MonsterAnimBase.h
+++ Source/HIVE/Gameplay/Monster/MonsterAnimBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameFramework/Character.h"
 #include "MonsterAnimBase.generated.h"
 
 /**
@@ -14,4 +15,12 @@ class HIVE_API UMonsterAnimBase : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(BlueprintReadOnly)
+		float ForwardAxis = 0.0f;
+	UPROPERTY(BlueprintReadOnly)
+		float RightAxis = 0.0f;
+
+public:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 };
